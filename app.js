8962a6ef946317db96b28cbdd90b2f61178df938
Diego <diego17almoradi@gmail.com>
2020-12-26 20:26:46 +0100

    Primer commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const { crearArchivo, listar } = require('./multiplicar/multiplicar');
+
+const argv = require('yargs')
+    .command('listar', 'Imprime en consola la tabla de multiplicar', {
+        base: {
+            demand: true,
+            alias: 'b'
+        },
+        limite: {
+            alias: 'l',
+            default: 10
+        }
+    })
+    .command('crear', 'Crea un fuchero con la tabla', {
+        base: {
+            demand: true,
+            alias: 'b'
+        },
+        limite: {
+            alias: 'l',
+            default: 10
+        }
+    })
+    .argv;
+
+let comando = argv._[0];
+
+switch (comando) {
+    case 'listar':
+        listar(argv.base, argv.limite);
+        break;
+    case 'crear':
+        crearArchivo(argv.base, argv.limite)
+            .then(archivo => console.log(archivo))
+            .catch(err => console.log(err));
+        break;
+    default:
+        console.log('Comando no reconocido');
+}
---
+++ multiplicar/multiplicar.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+let listar = (base, limite) => {
+    let data = '';
+
+    for (let i = 1; i <= limite; i++) {
+        data += `${base} * ${i} = ${base*i}\n`;
+    }
+
+    console.log(data);
+}
+
+let crearArchivo = (base, limite) => {
+    return new Promise((resolve, reject) => {
+        let data = '';
+
+        for (let i = 1; i <= limite; i++) {
+            data += `${base} * ${i} = ${base*i}\n`;
+        }
+
+        fs.writeFile(`tablas/tabla-${base}.txt`, data, (err) => {
+            if (err) reject(err);
+            else resolve(`tabla-${base}.txt`);
+        });
+    });
+}
+
+module.exports = {
+    crearArchivo,
+    listar
+}
